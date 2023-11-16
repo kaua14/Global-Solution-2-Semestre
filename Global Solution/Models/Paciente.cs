6526c6ef7bb73bed63446e555956c250ed381b43
logonrmlocal <logonrmlocal@PA402MICRO21>
2023-11-16 08:08:16 -0300

    Add project files.

---
+++ Global Solution/Models/Paciente.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Global_Solution.Models
+{
+    public class Paciente
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required] 
+        public string Nome { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Cpf { get; set; }
+
+    }
+}
