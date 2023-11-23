83253ec23e3df738baad457c7a03c8860c0883dc
kaua14 <rm94669@fiap.com.br>
2023-11-23 08:16:43 -0300

    Controllers de consulta e pagamento adicionadas ao projeto

---
+++ Global Solution/Controllers/cy_pagamentosController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Global_Solution.Models;
+
+namespace Global_Solution.Controllers
+{
+    public class cy_pagamentosController : Controller
+    {
+        private readonly OracleDBContext _context;
+
+        public cy_pagamentosController(OracleDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: cy_pagamentos
+        public async Task<IActionResult> Index()
+        {
+              return _context.cy_Pagamentos != null ? 
+                          View(await _context.cy_Pagamentos.ToListAsync()) :
+                          Problem("Entity set 'OracleDBContext.cy_Pagamentos'  is null.");
+        }
+
+        // GET: cy_pagamentos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.cy_Pagamentos == null)
+            {
+                return NotFound();
+            }
+
+            var cy_pagamentos = await _context.cy_Pagamentos
+                .FirstOrDefaultAsync(m => m.IdPagamento == id);
+            if (cy_pagamentos == null)
+            {
+                return NotFound();
+            }
+
+            return View(cy_pagamentos);
+        }
+
+        // GET: cy_pagamentos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: cy_pagamentos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdPagamento,valor,dataPagamento,status,informacoesCartao")] cy_pagamentos cy_pagamentos)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cy_pagamentos);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cy_pagamentos);
+        }
+
+        // GET: cy_pagamentos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.cy_Pagamentos == null)
+            {
+                return NotFound();
+            }
+
+            var cy_pagamentos = await _context.cy_Pagamentos.FindAsync(id);
+            if (cy_pagamentos == null)
+            {
+                return NotFound();
+            }
+            return View(cy_pagamentos);
+        }
+
+        // POST: cy_pagamentos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdPagamento,valor,dataPagamento,status,informacoesCartao")] cy_pagamentos cy_pagamentos)
+        {
+            if (id != cy_pagamentos.IdPagamento)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cy_pagamentos);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!cy_pagamentosExists(cy_pagamentos.IdPagamento))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cy_pagamentos);
+        }
+
+        // GET: cy_pagamentos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.cy_Pagamentos == null)
+            {
+                return NotFound();
+            }
+
+            var cy_pagamentos = await _context.cy_Pagamentos
+                .FirstOrDefaultAsync(m => m.IdPagamento == id);
+            if (cy_pagamentos == null)
+            {
+                return NotFound();
+            }
+
+            return View(cy_pagamentos);
+        }
+
+        // POST: cy_pagamentos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.cy_Pagamentos == null)
+            {
+                return Problem("Entity set 'OracleDBContext.cy_Pagamentos'  is null.");
+            }
+            var cy_pagamentos = await _context.cy_Pagamentos.FindAsync(id);
+            if (cy_pagamentos != null)
+            {
+                _context.cy_Pagamentos.Remove(cy_pagamentos);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool cy_pagamentosExists(int id)
+        {
+          return (_context.cy_Pagamentos?.Any(e => e.IdPagamento == id)).GetValueOrDefault();
+        }
+    }
+}
--- Global Solution/Models/OracleDBContext.cs
+++ Global Solution/Models/OracleDBContext.cs
@@ -4,27 +4,16 @@ namespace Global_Solution.Models
 {
     public class OracleDBContext : DbContext
     {
-        public DbSet<cy_consulta_medica> cy_consulta_medica { get; set; }
-
-        public DbSet<cy_especialidade_medica> cy_especialidade_medica { get; set; }
-
-        public DbSet<cy_feedbacks> cy_feedbacks { get; set; }
-
-        public DbSet<cy_historico_medico> cy_historico_medico { get; set; }
-
-
+        public DbSet<cy_consulta_medica> cy_ConsultaMedica { get; set; }
+        public DbSet<cy_especialidade_medica> cy_EspecialidadesMedicas { get; set; }
+        public DbSet<cy_feedbacks> cy_Feedbacks { get; set; }
+        public DbSet<cy_historico_medico> cy_HistoricoMedico { get; set; }
         public DbSet<cy_hospitais> cy_Hospitais { get; set; }
-
         public DbSet<cy_mensagens> cy_Mensagens { get; set; }
-
         public DbSet<cy_notificacoes> cy_Notificacoes { get; set; }
-
         public DbSet<cy_pagamentos> cy_Pagamentos { get; set; }
-
-        public DbSet<cy_prescricao_medica> cy_Prescricao_Medicas { get; set; }
-
-        public DbSet<cy_profissionais_saude> cy_Profissionais_Saudes { get; set; }
-
+        public DbSet<cy_prescricao_medica> cy_PrescricoesMedicas { get; set; }
+        public DbSet<cy_profissionais_saude> cy_ProfissionaisSaude { get; set; }
         public DbSet<cy_usuarios> cy_Usuarios { get; set; }
 
         public OracleDBContext(DbContextOptions<OracleDBContext> options) : base(options)
@@ -33,3 +22,5 @@ namespace Global_Solution.Models
         }
     }
 }
+
+
